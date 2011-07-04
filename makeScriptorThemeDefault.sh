2d2d108e22ae18a86f45ed6b77249a6dd854a80f
shadowc <info@matiasjose.com>
2011-07-04 05:51:38 -0300

    gone to using lessphp to generate css

--- makeScriptorThemeDefault.sh
+++ makeScriptorThemeDefault.sh
@@ -1,12 +1,3 @@
 #!/bin/sh
 
-rm default.css
-
-cat css/default/core.css			>> default.css
-cat css/default/contextMenu.css		>> default.css
-cat css/default/calendarView.css	>> default.css
-cat css/default/dataView.css		>> default.css
-cat css/default/galleryView.css		>> default.css
-cat css/default/treeView.css		>> default.css
-cat css/default/component.css		>> default.css
-cat css/default/tabContainer.css	>> default.css
+lessphp/plessc css/default.less > default.css
