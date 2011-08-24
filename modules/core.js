d2b70d65cad698ba782ff35b01e789e5a49e64e8
ShadowC <info@matiasjose.com>
2011-08-24 02:37:27 -0700

    version change

--- modules/core.js
+++ modules/core.js
@@ -4,7 +4,7 @@ var Scriptor = {
 	version : {
 		major : 2,
 		minor : 0,
-		instance : "alpha 5",
+		instance : "alpha 6",
 		toString : function() {
 			return this.major + "." + this.minor + " " + this.instance;
 		}
